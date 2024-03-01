6bafaba9d4c79235e2f79486afe3e4d59e1442fd
Hypoiodous <125425798+Hypoiodous@users.noreply.github.com>
2024-02-29 22:29:26 -0600

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,8 @@
 
-
 export function Footer() {
   return (
     <footer>
       <div className="mt-8 flex h-24 flex-col items-center justify-center p-2">
-        <Logo />
       </div>
     </footer>
   );
